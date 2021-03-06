24ba09af2fd77a57f7faca5208fc57a34828d643
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-03-06 17:23:26 -0500

    PythonBasics 3 - added logic to end_with_consonant function

--- PythonBasics3/pythonBasics3.py
+++ PythonBasics3/pythonBasics3.py
@@ -7,6 +7,7 @@
 # import regular expression module
 import re
 
+
 # Part A. ends_with_consonant(s)
 # Define a function ends_with_consonant(s) that takes a string and returns true
 # if it ends with a consonant and false otherwise.
@@ -14,21 +15,21 @@ import re
 # Note: Be sure to use RegEx and it works for both upper and lower case and for nonletters!
 
 def ends_with_consonant(s):
+    if re.search("[^aeiouAEIOU]$", s):
+        return True
+
+    return False
 
 
-  return
-
-
-
-
- # Part B. ends_with_number
+# Part B. ends_with_number
 # Define a function ends _with_number(s) that takes a string and returns true
 # if it ends with a number and false otherwise.
 # (For our purposes, a number is any character that is 0,1,2,3,4,5,6,7,8, or 9.)
 # Note: Be sure to use RegEx!
 def ends_with_number(s):
-
-  return
+    if re.search("[0-9]$", s):
+        return True
+    return False
 
 
 # Part C. binary_multiple_of_6
@@ -37,6 +38,4 @@ def ends_with_number(s):
 # Note: Be sure it returns false if the string is not a valid binary number!
 # Hint: Use regular expressions to match for the pattern of a binary number that is a multiple of 6.
 def binary_multiple_of_6(s):
-
-
-  return
+    return
