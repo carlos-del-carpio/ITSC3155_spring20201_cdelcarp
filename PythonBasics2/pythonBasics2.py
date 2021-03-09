eacce584ec0a413c963e9bac1e6d8db0adbd0b5d
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-03-09 13:56:16 -0500

    PythonBasics2 - Update methods

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -11,43 +11,70 @@
 # to n (including n).
 
 def count_threes(n):
-  count = 0
-  if (n > 0):
-    for num in range(1, n + 1):
-      if num % 3 == 0:
-        count += 1
+    dictionary = {}
+    highest_count = 0
+    returnNum = ""
 
-  return count
+    for num in n:
+        if num not in dictionary:
+            dictionary[num] = 0
+        dictionary[num] += 1
+
+    for key in dictionary:
+        if dictionary[key] > highest_count:
+            highest_count = dictionary[key]
+            returnNum = key
+
+    return int(returnNum)
 
 
 # Part B. longest_consecutive_repeating_char
 # Define a function longest_consecutive_repeating_char(s) that takes
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
-  repeating_letter = ""
-  repeating_count = 0
-  current_letter = ""
-  current_count = 0
-  for letter in s:
-    if repeating_letter == "":
-      repeating_letter = letter
-      repeating_count += 1
-      current_letter = letter
-      current_count + 1
+    # repeating_letter = ""
+    # repeating_count = 0
+    # current_letter = ""
+    # current_count = 0
+    # for letter in s:
+    #     if repeating_letter == "":
+    #         repeating_letter = letter
+    #         repeating_count += 1
+    #         current_letter = letter
+    #         current_count + 1
+    #
+    #     if letter == current_letter:
+    #         current_count += 1
+    #     else:
+    #         if current_count >= repeating_count:
+    #             repeating_letter = current_letter
+    #             repeating_count = current_count
+    #         current_letter = letter
+    #         current_count = 1
+    #
+    #     if current_count >= repeating_count:
+    #         repeating_letter = current_letter
+    #         repeating_count = current_count
+    dictionary = {}
+    returning_list = []
+    previous_letter = ""
 
-    if letter == current_letter:
-      current_count += 1
-    else:
-      if current_count >= repeating_count:
-        repeating_letter = current_letter
-        repeating_count = current_count
-      current_letter = letter
-      current_count = 1
+    for letter in s:
+        if letter == previous_letter:
+            dictionary[letter] += 1
+        elif letter not in dictionary:
+            dictionary[letter] = 1
 
-    if current_count >= repeating_count:
-      repeating_letter = current_letter
-      repeating_count = current_count
-  return repeating_letter
+        previous_letter = letter
+
+    all_values = dictionary.values()
+    max_value = max(all_values)
+
+    for key in dictionary:
+        if dictionary[key] == max_value:
+            returning_list.append(key)
+
+    return returning_list
 
 
 # Part C. is_palindrome
@@ -57,9 +84,9 @@ def longest_consecutive_repeating_char(s):
 # forwards. Treat capital letters the same as lowercase ones
 # and ignore spaces (i.e. case insensitive).
 def is_palindrome(s):
-  string = s.upper().replace(' ', '')
+    string = s.upper().replace(' ', '')
 
-  for index in range(len(string)):
-    if (string[index] != string[len(string) - 1 - index]):
-      return False
-  return True
+    for index in range(len(string)):
+        if (string[index] != string[len(string) - 1 - index]):
+            return False
+    return True
