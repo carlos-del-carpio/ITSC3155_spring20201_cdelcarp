4f661c7f7eeb5a79fa268d02b6f3b4c260115e4c
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-03-20 13:36:16 -0400

    flask app #3 - added execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -2,6 +2,7 @@
 import os                # os is used to get environment variables IP & PORT
 from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)  # create an app
 
@@ -22,7 +23,7 @@ def get_notes():
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-02-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-03-2020'}}
     a_user = {'name': 'Carlos', 'email': 'mogli@uncc.edu'}
-    return render_template('notes.html', notes=notes, user=a_user);
+    return render_template('notes.html', notes=notes, user=a_user)
 
 
 @app.route('/notes/<note_id>')
@@ -34,9 +35,12 @@ def get_note(note_id):
     return render_template("note.html", note=notes[int(note_id)], user=a_user)
 
 
-@app.route('/notes/new')
+@app.route('/notes/new', methods=["GET", "POST"])
 def new_note():
     a_user = {'name': 'Carlos', 'email': 'mogli@uncc.edu'}
+    print('request method is ', request.method)
+    if request.method == 'POST':
+        return '<h1> POST method used for this request </h1>'
     return render_template('new.html', user=a_user)
 
 
